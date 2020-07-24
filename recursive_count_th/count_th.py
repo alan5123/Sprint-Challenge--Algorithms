bd18fa2310ce1948dd69495d7602b606ea087d88
alan5123 <alanparker5123@gmail.com>
2020-07-24 15:42:39 -0400

    new branch

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -5,10 +5,11 @@ Your function must utilize recursion. It cannot contain any loops.
 '''
 def count_th(word):
     
-        
+         # there can't be a match cuz we are looking for 2 letters   
     if len(word) < 2:
         return 0
     if word[0] + word[1] == "th":
+         # recursively call the list excluding the last two, makes the array smaller
         return 1 + count_th(word[1:])
     else:
         return 0 + count_th(word[1:])
